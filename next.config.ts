5a8927288ce8925336bf81e6fa9eb4483bee3324
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-01-15 13:50:13 +0200

    feat: mail, animecard, next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flxt.tmsimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
